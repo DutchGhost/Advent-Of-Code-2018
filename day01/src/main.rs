df262f65a4255666685b5d33b355ba0abd9a106d
Dodo <kasper199914@gmail.com>
2018-12-01 07:02:27 +0100

    day 1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,10 @@
+const PUZZLE: &str = include_str!("input.txt");
+
 fn main() {
-    println!("Hello, world!");
+    let frequency = PUZZLE
+        .lines()
+        .filter_map(|s| s.parse::<isize>().ok())
+        .sum::<isize>();
+
+    println!("{}", frequency);
 }
--- day01_2/src/main.rs
+++ day01_2/src/main.rs
@@ -1,3 +1,19 @@
+const PUZZLE: &str = include_str!("input.txt");
+use std::collections::HashSet;
+
 fn main() {
-    println!("Hello, world!");
+    let mut set = HashSet::new();
+
+    let mut frequency = 0;
+    for number in PUZZLE.lines().flat_map(|s| s.parse::<isize>().ok()).cycle() {
+        frequency += number;
+
+        if set.contains(&frequency) {
+            break;
+        }
+
+        set.insert(frequency);
+    }
+
+    println!("{:?}", frequency);
 }
