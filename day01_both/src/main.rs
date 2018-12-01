24ddfd72cff8194132864552d9131c9d4d2415e0
Dodo <kasper199914@gmail.com>
2018-12-01 15:21:46 +0100

    use itertools update

--- day01_both/src/main.rs
+++ day01_both/src/main.rs
@@ -1,5 +1,9 @@
 #![feature(cell_update)]
 
+extern crate itertools;
+
+use itertools::Itertools;
+
 const PUZZLE: &str = include_str!("input.txt");
 
 use std::{cell::Cell, collections::HashSet};
@@ -14,14 +18,17 @@ fn main() {
     let finalsum = *cumulative_sums.last().unwrap();
 
     let set = cumulative_sums.iter().cloned().collect::<HashSet<_>>();
-    println!("part1: {:?} part2: {:?}", finalsum, iterate(&mut cumulative_sums, &set, finalsum));
-}
 
-fn iterate(sums: &mut [isize], set: &HashSet<isize>, finalsum: isize) -> isize {
-    sums.iter_mut().for_each(|n| *n += finalsum);
-
-    match sums.iter().find(|n| set.contains(n)) {
-        Some(n) => *n,
-        None => iterate(sums, set, finalsum),
-    }
+    let part2 = loop {
+        match cumulative_sums
+            .iter_mut()
+            .update(|n| {
+                **n += finalsum;
+            }).find(|n| set.contains(n))
+        {
+            Some(n) => break *n,
+            _ => continue,
+        }
+    };
+    println!("part1: {:?} part2: {:?}", finalsum, part2);
 }
