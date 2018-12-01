d81cfdbc92c77f45dbf2141ff5676b480fe50a11
Dodo <kasper199914@gmail.com>
2018-12-01 07:29:29 +0100

    use cell, and hashset's insert return value

--- day01_2/src/main.rs
+++ day01_2/src/main.rs
@@ -1,19 +1,22 @@
+#![feature(cell_update)]
+
 const PUZZLE: &str = include_str!("input.txt");
+use std::cell::Cell;
 use std::collections::HashSet;
 
 fn main() {
     let mut set = HashSet::new();
 
-    let mut frequency = 0;
-    for number in PUZZLE.lines().flat_map(|s| s.parse::<isize>().ok()).cycle() {
-        frequency += number;
+    let frequency = Cell::new(0);
 
-        if set.contains(&frequency) {
-            break;
-        }
-
-        set.insert(frequency);
-    }
+    PUZZLE
+        .lines()
+        .flat_map(|s| s.parse::<isize>().ok())
+        .cycle()
+        .take_while(|_| set.insert(frequency.get()))
+        .for_each(|n| {
+            frequency.update(|old| old + n);
+        });
 
     println!("{:?}", frequency);
 }
