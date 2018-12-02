b759d47054628553efdee3c80115acdcf4269218
Dodo <kasper199914@gmail.com>
2018-12-02 12:24:06 +0100

    use slices instead of making a new String

--- day02_2/src/main.rs
+++ day02_2/src/main.rs
@@ -1,36 +1,48 @@
 const PUZZLE: &str = include_str!("input.txt");
 
-use std::{
-    collections::HashMap,
-};
+#[derive(Default)]
+struct IDMatcher<'a> {
+    s1: &'a str,
+    s2: &'a str,
+}
+
+impl<'a> IDMatcher<'a> {
+    /// Returns Some(Self) if all characters in `s1` and `s2` are equal,
+    /// or if all but 1 character are equal.
+    /// Returns None otherwise.
+    pub fn find_match(s1: &'a str, s2: &'a str) -> Option<Self> {
+        let mut iter = s1.chars().zip(s2.chars());
+        let equal_count = iter.by_ref().take_while(|(c1, c2)| c1 == c2).count();
+
+        // all chars are equal
+        if equal_count == s1.len() {
+            return Some(Self { s1, s2: "" });
+        }
+
+        let equal_count_tail = iter.take_while(|(c1, c2)| c1 == c2).count();
+
+        // all but one are equal
+        if equal_count + equal_count_tail == s1.len() - 1 {
+            return Some(Self {
+                s1: &s1[..equal_count],
+                s2: &s1[equal_count + 1..],
+            });
+        }
+        None
+    }
+}
 
 fn main() {
     let boxes = PUZZLE.lines().collect::<Vec<_>>();
 
-    let mut common = String::new();
+    let common = boxes
+        .iter()
+        .enumerate()
+        .find_map(|(idx, box1)| {
+            boxes[idx + 1..]
+                .iter()
+                .find_map(|box2| IDMatcher::find_match(box1, box2))
+        }).expect("Failed to find it.");
 
-    'outer: for (idx, b1) in boxes.iter().enumerate() {
-        for b2 in boxes[idx..].iter() {
-            let mut faults = 0;
-
-            for(c1, c2) in b1.chars().zip(b2.chars()) {
-                if c1 != c2 {
-                    faults += 1;
-                } else {
-                    common.push(c1);
-                }
-
-                if faults > 1 {
-                    break;
-                }
-            }
-
-            if faults == 1 {
-                break 'outer;
-            }
-            common.clear();
-        }
-    }
-
-    println!("{}", common);
+    println!("{}{}", common.s1, common.s2);
 }
