006515154b72957ba66dfdbfbbb6cf9ef31d2562
DutchGhost <kasper199914@gmail.com>
2018-12-02 17:01:33 +0100

    add zig

---
+++ Zig/day01/src/main.zig
@@ -0,0 +1,24 @@
+const std = @import("std");
+const mem = std.mem;
+const fmt = std.fmt;
+
+const input = @embedFile("../input.txt");
+
+fn solve(comptime N: type) N {
+    var splitter = mem.split(input, "\n");
+    var sum = N(0);
+
+    while(splitter.next()) |n| {
+        var num = fmt.parseInt(N, n, 10) catch unreachable;
+        sum += num;
+    }
+
+    return sum;
+}
+
+pub fn main() void {
+    @setEvalBranchQuota(500000); 
+    comptime var answer = comptime solve(i32);
+
+    std.debug.warn("part 1: {}", answer);    
+}
