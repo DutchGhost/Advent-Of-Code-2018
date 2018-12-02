6b2a9a869577b8bad3ac091a8ef0c8d5247a3082
Dodo <kasper199914@gmail.com>
2018-12-02 09:03:07 +0100

    day 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,3 +1,30 @@
+const PUZZLE: &str = include_str!("input.txt");
+
+use std::{
+    collections::HashMap,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let mut frequencies = HashMap::new();
+    let mut v = Vec::new();
+
+    let mut twos = 0;
+    let mut threes = 0;
+
+    for line in PUZZLE.lines() {
+        for c in line.chars() {
+            *frequencies.entry(c).or_insert(0) += 1;
+        }
+
+        v.extend(frequencies.drain().map(|(_, v)| v));
+
+        v.retain(|n| *n == 2 || *n == 3);
+        v.dedup();
+
+        for n in v.drain(..) {
+            if n == 2 { twos += 1 } else { threes += 1 }
+        }
+    }
+
+    println!("{}", twos * threes);
 }
--- day02_2/src/main.rs
+++ day02_2/src/main.rs
@@ -1,3 +1,36 @@
+const PUZZLE: &str = include_str!("input.txt");
+
+use std::{
+    collections::HashMap,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let boxes = PUZZLE.lines().collect::<Vec<_>>();
+
+    let mut common = String::new();
+
+    'outer: for (idx, b1) in boxes.iter().enumerate() {
+        for b2 in boxes[idx..].iter() {
+            let mut faults = 0;
+
+            for(c1, c2) in b1.chars().zip(b2.chars()) {
+                if c1 != c2 {
+                    faults += 1;
+                } else {
+                    common.push(c1);
+                }
+
+                if faults > 1 {
+                    break;
+                }
+            }
+
+            if faults == 1 {
+                break 'outer;
+            }
+            common.clear();
+        }
+    }
+
+    println!("{}", common);
 }
