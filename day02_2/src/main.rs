16ab530d15a4175e989f29aa9a11a8ead99eda90
Dodo <kasper199914@gmail.com>
2018-12-02 12:49:12 +0100

    remove unreachable case

--- day02_2/src/main.rs
+++ day02_2/src/main.rs
@@ -7,27 +7,19 @@ struct IDMatcher<'a> {
 }
 
 impl<'a> IDMatcher<'a> {
-    /// Returns Some(Self) if all characters in `s1` and `s2` are equal,
-    /// or if all but 1 character are equal.
-    /// Returns None otherwise.
+    /// Returns `Some(Self)` if the 2 boxes are correct boxes, a.k.a, they differ only 1 character.
     pub fn find_match(s1: &'a str, s2: &'a str) -> Option<Self> {
         let mut iter = s1.chars().zip(s2.chars());
         let equal_count = iter.by_ref().take_while(|(c1, c2)| c1 == c2).count();
-
-        // all chars are equal
-        if equal_count == s1.len() {
-            return Some(Self { s1, s2: "" });
-        }
-
         let equal_count_tail = iter.take_while(|(c1, c2)| c1 == c2).count();
 
-        // all but one are equal
         if equal_count + equal_count_tail == s1.len() - 1 {
             return Some(Self {
                 s1: &s1[..equal_count],
                 s2: &s1[equal_count + 1..],
             });
         }
+
         None
     }
 }
@@ -38,11 +30,12 @@ fn main() {
     let common = boxes
         .iter()
         .enumerate()
-        .find_map(|(idx, box1)| {
-            boxes[idx + 1..]
+        .filter_map(|(idx, box1)| {
+            boxes[idx..]
                 .iter()
                 .find_map(|box2| IDMatcher::find_match(box1, box2))
-        }).expect("Failed to find it.");
+        }).next()
+        .expect("Failed to find it.");
 
     println!("{}{}", common.s1, common.s2);
 }
