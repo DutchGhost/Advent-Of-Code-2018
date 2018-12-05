d1e3691e6866eb33f8108358e9af5ff1a3d88f3b
Dodo <kasper199914@gmail.com>
2018-12-06 00:22:41 +0100

    build iterator in seperate function

--- day05_2/src/main.rs
+++ day05_2/src/main.rs
@@ -20,13 +20,17 @@ fn react(input: impl Iterator<Item = char>, stack: &mut String) -> usize {
     stack.len()
 }
 
-fn is_filter_char(c: char, filter_char: char) -> bool {
+fn is_unit_char(c: char, filter_char: char) -> bool {
     c == filter_char || c == filter_char.to_ascii_uppercase()
+}
+
+fn filter_units<'a>(s: &'a str, unit: char) -> impl Iterator<Item = char> + 'a {
+    s.chars().filter(move |&c| !is_unit_char(c, unit))
 }
 
 use std::time::Instant;
 
-#[aoc(2018, 5, 1)]
+#[aoc(2018, 5, 2)]
 fn main(input: &str) -> usize {
     let now = Instant::now();
 
@@ -34,8 +38,8 @@ fn main(input: &str) -> usize {
     let mut len = std::usize::MAX;
     let mut buffer = String::new();
 
-    for filter_char in (b'a'..=b'z').map(|c| c as char) {
-        let iter = input.chars().filter(|&c| !is_filter_char(c, filter_char));
+    for unit_char in (b'a'..=b'z').map(|c| c as char) {
+        let iter = filter_units(input, unit_char);
         len = std::cmp::min(len, react(iter, &mut buffer));
         buffer.clear();
     }
