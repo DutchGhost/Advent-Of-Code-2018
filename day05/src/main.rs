bfcc1bdd960a5b6a65aee42bf0d18798eaa853c6
Dodo <kasper199914@gmail.com>
2018-12-05 14:16:14 +0100

    use stack based algorithm

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,28 +1,27 @@
 use aoc::aoc;
 
-#[aoc(2018, 5, 1)]
-fn main(input: &str) -> usize {
-    let mut input = input.trim().to_owned();
-    let mut reacting_units = Vec::new();
+fn complement(c: char) -> char {
+    if c.is_uppercase() {
+        c.to_ascii_lowercase()
+    } else {
+        c.to_ascii_uppercase()
+    }
+}
 
-    for c in (97..=122u8).map(|b| b as char) {
-        let lower_upper = format!("{}{}", c, c.to_ascii_uppercase());
-        let upper_lower = format!("{}{}", c.to_ascii_uppercase(), c);
-
-        reacting_units.push(lower_upper);
-        reacting_units.push(upper_lower);
+fn react(input: impl Iterator<Item = char>) -> usize {
+    let mut stack = String::new();
+    for c in input {
+        match stack.chars().last().filter(|last| *last == complement(c)) {
+            // Need the drop here to make the match arm happy.
+            Some(_) => drop(stack.pop()),
+            None => stack.push(c),
+        }
     }
 
-    (0..)
-        .map(|_| reacting_units.iter())
-        .map(|iter| {
-            let len_before = input.len();
-            iter.for_each(|unit| input = input.replace(unit, ""));
-            let len_after = input.len();
+    stack.len()
+}
 
-            (len_after - len_before, len_after)
-        }).take_while(|(len, _)| *len != 0)
-        .last()
-        .map(|(_, len)| len)
-        .unwrap()
+#[aoc(2018, 5, 1)]
+fn main(input: &str) -> usize {
+    react(input.trim().chars())
 }
--- day05_2/src/main.rs
+++ day05_2/src/main.rs
@@ -1,36 +1,42 @@
 use aoc::aoc;
 
-fn poly(mut to_react: String, reacting_units: &[String]) -> usize {
-    (0..)
-        .map(|_| reacting_units.iter())
-        .map(|iter| {
-            let len_before = to_react.len();
-            iter.for_each(|unit| to_react = to_react.replace(unit, ""));
-            let len_after = to_react.len();
+fn complement(c: char) -> char {
+    if c.is_uppercase() {
+        c.to_ascii_lowercase()
+    } else {
+        c.to_ascii_uppercase()
+    }
+}
 
-            (len_after - len_before, len_after)
-        }).take_while(|(len, _)| *len != 0)
-        .last()
-        .map(|(_, len)| len)
-        .unwrap()
+fn react(input: impl Iterator<Item = char>, stack: &mut String) -> usize {
+    for c in input {
+        match stack.chars().last().filter(|last| *last == complement(c)) {
+            // Need the drop here to make the match arm happy.
+            Some(_) => drop(stack.pop()),
+            None => stack.push(c),
+        }
+    }
+
+    stack.len()
+}
+
+fn is_filter_char(c: char, filter_char: char) -> bool {
+    c == filter_char || c == filter_char.to_ascii_uppercase()
 }
 
 #[aoc(2018, 5, 1)]
 fn main(input: &str) -> Option<usize> {
     let input = input.trim();
-    let mut reacting_units = Vec::new();
-
-    for c in (97..=122u8).map(|b| b as char) {
-        let lower_upper = format!("{}{}", c, c.to_ascii_uppercase());
-        let upper_lower = format!("{}{}", c.to_ascii_uppercase(), c);
-
-        reacting_units.push(lower_upper);
-        reacting_units.push(upper_lower);
-    }
+    let mut buffer = String::new();
 
     (97..=122u8)
         .map(|b| b as char)
-        .map(|c| input.replace(c, "").replace(c.to_ascii_uppercase(), ""))
-        .map(|s| poly(s, &reacting_units))
-        .min()
+        .map(|filter_char| {
+            let len = react(
+                input.chars().filter(|c| !is_filter_char(*c, filter_char)),
+                &mut buffer,
+            );
+            buffer.clear();
+            len
+        }).min()
 }
