2177f775f901cb6c33fe310706cfb85012587d50
Dodo <kasper199914@gmail.com>
2018-12-07 21:56:44 +0100

    rewrite part 1 to strategy used in part 2. Also rename things like 'pre and post' to 'dependency and task'

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -3,40 +3,34 @@ use aoc::aoc;
 use std::collections::{HashMap, HashSet};
 
 fn parse(s: &str) -> (char, char) {
-    let mut pre = s.chars().skip(5);
-    let mut post = s.chars().skip(36);
+    let mut dependency = s.chars().skip(5);
+    let mut task = s.chars().skip(36);
 
-    (pre.next().unwrap(), post.next().unwrap())
+    (dependency.next().unwrap(), task.next().unwrap())
 }
 
-fn solve(mut instructions: HashMap<char, HashSet<char>>) -> String {
+fn solve(mut tasks: HashMap<char, HashSet<char>>) -> String {
     let mut answer = String::new();
-
-    let mut steps = HashSet::new();
-
-    for (pre, posts) in instructions.iter() {
-        steps.insert(*pre);
-        steps.extend(posts.iter().cloned());
-    }
-
-    let mut posts = HashSet::new();
     let mut candidates: Vec<char> = Vec::new();
 
-    while !steps.is_empty() {
-        for (_, post) in instructions.iter() {
-            posts.extend(post.iter().cloned());
-        }
+    while !tasks.is_empty() {
+        candidates.extend(
+            tasks
+                .iter()
+                .filter(|(_, dependencies)| dependencies.is_empty())
+                .map(|(task, _)| task.clone()),
+        );
 
-        candidates.extend(steps.difference(&posts).cloned());
         candidates.sort();
-        let n = candidates[0];
+        let task = candidates[0];
         candidates.clear();
-        answer.push(n);
+        answer.push(task);
 
-        instructions.remove(&n);
+        tasks.remove(&task);
 
-        steps.remove(&n);
-        posts.clear();
+        for (_, dependencies) in &mut tasks {
+            dependencies.remove(&task);
+        }
     }
 
     answer
@@ -44,12 +38,35 @@ fn solve(mut instructions: HashMap<char, HashSet<char>>) -> String {
 
 #[aoc(2018, 7, 1)]
 fn main(input: &str) -> String {
-    let mut map = HashMap::new();
+    let mut task_dependency_map = HashMap::new();
 
+    // add tasks and dependencies to the task_dependency_map
     for line in input.lines() {
-        let (pre, post) = parse(line);
+        let (dependency, task) = parse(line);
 
-        map.entry(pre).or_insert(HashSet::new()).insert(post);
+        task_dependency_map
+            .entry(task)
+            .or_insert(HashSet::new())
+            .insert(dependency);
     }
-    solve(map)
+
+    let mut are_dependencies_only = Vec::new();
+
+    // find the things which are not tasks, but *only* a dependency
+    for (_, dependencies) in task_dependency_map.iter() {
+        for dependency in dependencies.iter() {
+            if !task_dependency_map.contains_key(dependency) {
+                are_dependencies_only.push(*dependency);
+            }
+        }
+    }
+
+    // The ones which are dependency only, but not a task, depend on an empty dependency list.
+    for dependency in are_dependencies_only {
+        task_dependency_map
+            .entry(dependency)
+            .or_insert(HashSet::new());
+    }
+
+    solve(task_dependency_map)
 }
--- day07_2/src/main.rs
+++ day07_2/src/main.rs
@@ -3,10 +3,10 @@ use aoc::aoc;
 use std::collections::{HashMap, HashSet};
 
 fn parse(s: &str) -> (char, char) {
-    let mut pre = s.chars().skip(5);
-    let mut post = s.chars().skip(36);
+    let mut dependency = s.chars().skip(5);
+    let mut task = s.chars().skip(36);
 
-    (pre.next().unwrap(), post.next().unwrap())
+    (dependency.next().unwrap(), task.next().unwrap())
 }
 
 #[derive(Debug)]
@@ -19,8 +19,9 @@ struct Worker {
 
 pub enum Poll<T> {
     Ready(T),
-    Working
+    NotReady,
 }
+
 impl Worker {
     fn new(default_ticks: usize) -> Self {
         Self {
@@ -29,7 +30,7 @@ impl Worker {
             default_ticks: default_ticks,
         }
     }
-    
+
     fn spawn(&mut self, c: char) {
         self.work = Some(c);
         self.ticks_left = (c as u8 - b'A') as usize + 1 + self.default_ticks;
@@ -37,11 +38,10 @@ impl Worker {
 
     pub fn poll(&mut self) -> Option<Poll<char>> {
         self.ticks_left = self.ticks_left.saturating_sub(1);
-        println!("{:?}", self.work);
         if self.ticks_left == 0 {
             self.work.take().map(|w| Poll::Ready(w))
         } else {
-            Some(Poll::Working)
+            Some(Poll::NotReady)
         }
     }
 }
@@ -51,40 +51,34 @@ fn solve(
     number_of_workers: usize,
     default_time: usize,
 ) -> usize {
-
     let mut workers = (0..number_of_workers)
         .map(|_| Worker::new(default_time))
         .collect::<Vec<_>>();
 
     let mut output = String::new();
-    let mut satisfied = HashSet::new();
     let mut spawnable_tasks = Vec::new();
 
     let mut tick = 0;
-    
+
     loop {
         tick += 1;
         let mut idles = Vec::new();
 
         for worker in workers.iter_mut() {
             match worker.poll() {
-
                 // Some task was completed.
                 // Push it to the output,
                 // Remove it as a dependency for all tasks that depend on the just completed task.
                 // This worker is now idle, push it to the idle workers.
                 Some(Poll::Ready(task_complete)) => {
-
                     output.push(task_complete);
-                    satisfied.insert(task_complete);
 
                     for (_, dependencies) in &mut tasks {
                         dependencies.remove(&task_complete);
                     }
 
                     idles.push(worker);
-
-                },
+                }
 
                 // this worker was idle, push it to the idles.
                 None => {
@@ -92,7 +86,7 @@ fn solve(
                 }
 
                 // this worker is still bussy. Let it do it's job
-                Some(Poll::Working) => {},
+                Some(Poll::NotReady) => {}
             }
         }
 
@@ -108,21 +102,21 @@ fn solve(
 
         // Find the next tasks to be spawned.
         // A task can be spawned if all its dependency's are satisfied.
-        spawnable_tasks.extend(tasks
-            .iter()
-            .filter(|(_, dependencies)| dependencies.iter().all(|dependency| satisfied.contains(dependency)))
-            .map(|(task, _)| *task));
-        
-        
+        spawnable_tasks.extend(
+            tasks
+                .iter()
+                .filter(|(_, dependencies)| dependencies.is_empty())
+                .map(|(task, _)| *task),
+        );
+
         spawnable_tasks.sort();
 
         // Spawn the tasks on the workers.
-        // Remove the work from the tasks, because we started working on it.
+        // Remove the task from the tasks, because we started working on it.
         for (worker, task) in idles.into_iter().zip(spawnable_tasks.drain(..)) {
             tasks.remove(&task);
             worker.spawn(task);
         }
-
     }
 
     return tick - 1;
@@ -130,30 +124,35 @@ fn solve(
 
 #[aoc(2018, 7, 2)]
 fn main(input: &str) -> usize {
-    let mut work_dependency_map = HashMap::new();
+    let mut task_dependency_map = HashMap::new();
 
-    // add work and dependencies to the work_dependency_map
+    // add tasks and dependencies to the task_dependency_map
     for line in input.lines() {
-        let(dependency, task) = parse(line);
+        let (dependency, task) = parse(line);
 
-        work_dependency_map.entry(task).or_insert(HashSet::new()).insert(dependency);
+        task_dependency_map
+            .entry(task)
+            .or_insert(HashSet::new())
+            .insert(dependency);
     }
 
     let mut are_dependencies_only = Vec::new();
 
-    // find the things which are not work, but *only* a dependency
-    for (_, dependencies) in work_dependency_map.iter() {
+    // find the things which are not tasks, but *only* a dependency
+    for (_, dependencies) in task_dependency_map.iter() {
         for dependency in dependencies.iter() {
-            if !work_dependency_map.contains_key(dependency) {
+            if !task_dependency_map.contains_key(dependency) {
                 are_dependencies_only.push(*dependency);
             }
         }
     }
 
-    // The ones which are dependency only, but not work, depend on an empty dependency list.
+    // The ones which are dependency only, but not a task, depend on an empty dependency list.
     for dependency in are_dependencies_only {
-        work_dependency_map.entry(dependency).or_insert(HashSet::new());
+        task_dependency_map
+            .entry(dependency)
+            .or_insert(HashSet::new());
     }
-  
-    solve(work_dependency_map, 5, 60)
+
+    solve(task_dependency_map, 5, 60)
 }
