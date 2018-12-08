2836a224fdbc9591966080d855e466c93bc645d0
Dodo <kasper199914@gmail.com>
2018-12-08 12:27:53 +0100

    day 8 part 2 with Iterators

--- day08_2/src/main.rs
+++ day08_2/src/main.rs
@@ -1,50 +1,24 @@
 use aoc::aoc;
 
-fn solve(mut iter: Vec<usize>) -> (usize, Vec<usize>) {
+fn solve(iter: &mut impl Iterator<Item = usize>) -> usize {
+    match (iter.next(), iter.next()) {
+        (Some(0), Some(meta_nodes)) => iter.take(meta_nodes).sum(),
 
-    let mut sum = 0;
-
-    let n_child_nodes = iter.pop().unwrap();
-    let n_meta_nodes = iter.pop().unwrap();
-    
-    if n_child_nodes == 0 {
-        for _ in 0..n_meta_nodes {
-            sum += iter.pop().unwrap();
+        (Some(child_nodes), Some(meta_nodes)) => {
+            let child_sums = (0..child_nodes).map(|_| solve(iter)).collect::<Vec<_>>();
+            iter.take(meta_nodes)
+                .filter_map(|idx| child_sums.get(idx - 1))
+                .sum()
         }
 
-        return (sum, iter);
+        _ => 0,
     }
-    
-    
-    let mut child_nodes_sum = Vec::new();
-    
-    for _ in 0..n_child_nodes {
-        let (child_node_sum, iter2) = solve(iter);
-        child_nodes_sum.push(child_node_sum);
-        iter = iter2;
-    }
-    
-    let mut metas = Vec::new();
-
-    for _ in 0..n_meta_nodes {
-        metas.push(iter.pop().unwrap());
-    }
-
-    for idx in metas {
-        if let Some(elem) = child_nodes_sum.get(idx - 1) {
-            sum += elem;
-        }
-    }
-
-    (sum, iter)
 }
 #[aoc(2018, 8, 2)]
 fn main(input: &str) -> usize {
     let mut input = input
         .split_whitespace()
-        .map(|s| s.parse::<usize>().unwrap())
-        .collect::<Vec<_>>();
+        .map(|s| s.parse::<usize>().unwrap());
 
-    input.reverse();
-    solve(input).0
+    solve(&mut input)
 }
