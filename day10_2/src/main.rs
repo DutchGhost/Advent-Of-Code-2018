af1371172acf00102848685dc79abda1548070ae
Dodo <kasper199914@gmail.com>
2018-12-10 14:48:44 +0100

    day 10

--- day10_2/src/main.rs
+++ day10_2/src/main.rs
@@ -1,4 +1,91 @@
 use aoc::aoc;
 
+use std::str::FromStr;
+
+macro_rules! Struct3 {
+    ($s:ident) => {
+        #[derive(Debug, Eq, PartialEq, Clone, Ord, PartialOrd)]
+        struct $s {
+            x: i64,
+            y: i64,
+        }
+        
+        impl FromStr for $s {
+            type Err = ();
+        
+            #[inline]
+            fn from_str(s: &str) -> Result<Self, Self::Err> {
+                let (x, y) = to_nums(s.chars());
+                Ok($s { x: x, y: y })
+            }
+        }
+    };
+}
+
+#[inline]
+fn to_nums<I: Iterator<Item = char>>(iter: I) -> (i64, i64) {
+    let stringified = iter
+        .filter(|c| c.is_digit(10) || c == &'-' || c == &',')
+        .collect::<String>();
+
+    let mut it = stringified.split(",");
+
+    let n1 = it.next().unwrap().parse::<i64>().unwrap();
+    let n2 = it.next().unwrap().parse::<i64>().unwrap();
+
+    (n1, n2)
+}
+
+Struct3!(Position);
+Struct3!(Velocity);
+
+struct Point {
+    position: Position,
+    velocity: Velocity,
+}
+
+fn parse(s: &str) -> Vec<Point> {
+    let mut points = Vec::new();
+
+    for line in s.lines() {
+        let mut split = line.split(" v");
+
+        let position = split.next().unwrap().parse::<Position>().unwrap();
+        let velocity = split.next().unwrap().parse::<Velocity>().unwrap();
+
+        points.push(Point {position, velocity});
+    }
+
+    points
+}
+
+use std::{
+    cmp::{min, max},
+};
+
 #[aoc(2018, 10, 2)]
-fn main(input: &str) {}
+fn main(input: &str) -> i64 {
+    let points = parse(input);
+
+    let (n, _) = (0..20000).map(|n| {
+
+        let mut minx = std::i64::MAX;
+        let mut maxx = 0;
+        let mut miny = std::i64::MAX;
+        let mut maxy = 0;
+
+        for p in points.iter() {
+            let x = p.position.x + n * p.velocity.x;
+            let y = p.position.y + n * p.velocity.y;
+
+            minx = min(minx, x);
+            maxx = max(maxx, x);
+            miny = min(miny, y);
+            maxy = max(maxy, y);
+        }
+
+        (n, maxx - minx + maxy - miny)
+    }).min_by_key(|&(_, bounds)| bounds).unwrap();
+
+    n
+}
