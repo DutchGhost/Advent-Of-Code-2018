0903ee581ae2d7c2bfd9844a204ebae67805e442
Dodo <kasper199914@gmail.com>
2018-12-16 18:43:10 +0100

    horrible part 2

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,4 +1,88 @@
 use aoc::aoc;
 
+#[derive(Debug)]
+pub struct Elf {
+    x: usize,
+    y: usize,
+    health: usize,
+}
+
+impl Elf {
+    pub fn new(x: usize, y: usize) -> Self {
+        Self { x, y, health: 200 }
+    }
+}
+
+#[derive(Debug)]
+pub struct Goblin {
+    x: usize,
+    y: usize,
+    health: usize,
+}
+
+impl Goblin {
+    pub fn new(x: usize, y: usize) -> Self {
+        Self { x, y, health: 200 }
+    }
+}
+
+#[derive(Debug)]
+pub enum Unit {
+    Elf(Elf),
+    Goblin(Goblin),
+}
+
+#[derive(Debug)]
+pub enum Node {
+    Wall,
+    Cavern,
+}
+
+impl From<char> for Node {
+    fn from(c: char) -> Self {
+        match c {
+            '#' => Node::Wall,
+            '.' => Node::Cavern,
+            _ => Node::Cavern,
+        }
+    }
+}
+
+pub type Area = Vec<Vec<Node>>;
+
+pub fn parse_area(s: &str) -> (Area, Vec<Unit>) {
+    let area = s
+        .lines()
+        .map(|row| row.chars().map(|cell| Node::from(cell)).collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let units = s
+        .lines()
+        .enumerate()
+        .flat_map(|(y, row)| {
+            row.chars()
+                .enumerate()
+                .filter(|(x, cell)| *cell == 'G' || *cell == 'E')
+                .map(move |(x, cell)| match cell {
+                    'G' => Unit::Goblin(Goblin::new(x, y)),
+                    _ => Unit::Elf(Elf::new(x, y)),
+                })
+        }).collect::<Vec<_>>();
+
+    (area, units)
+}
+
 #[aoc(2018, 15, 1)]
-fn main(input: &str) {}
+fn main(input: &str) {
+    let (area, units) = parse_area(input);
+
+    for row in area {
+        for cell in row {
+            match cell {
+                Node::Wall => print!("#"),
+                Node::Cavern => print!("."),
+            }
+        }
+        println!()
+    }
+}
--- day16_2/src/main.rs
+++ day16_2/src/main.rs
@@ -1,4 +1,314 @@
+#![feature(dbg_macro)]
 use aoc::aoc;
 
+macro_rules! assert_len {
+    ($e:expr) => {
+        match $e.len() {
+            4 => {}
+            len => {
+                dbg!($e);
+                panic!("Expected a slice with a len of 4, got len of {}", len);
+            }
+        }
+    };
+}
+
+macro_rules! opcode_impl_ops {
+    ([$fn_register:ident, $fn_immidiate:ident] -> $operation:tt $(,[$fn_registers:ident, $fn_immidiates:ident] -> $operations:tt)*) => {
+        fn $fn_register(instruction: &[usize], registers: &[usize]) -> [usize; 4] {
+            assert_len!(instruction);
+            assert_len!(registers);
+
+            let lhs = instruction[1];
+            let rhs = instruction[2];
+            let store = instruction[3];
+
+            let mut output = copy_register(registers);
+
+            output[store] = registers[lhs] $operation registers[rhs];
+            output
+        }
+
+        pub fn $fn_immidiate(instruction: &[usize], registers: &[usize]) -> [usize; 4] {
+            assert_len!(instruction);
+            assert_len!(registers);
+
+            let lhs = instruction[1];
+            let rhs = instruction[2];
+            let store = instruction[3];
+
+            let mut output = copy_register(registers);
+
+            output[store] = registers[lhs] $operation rhs;
+            output
+        }
+
+        opcode_impl_ops!($([$fn_registers, $fn_immidiates] -> $operations),*);
+    };
+    () => {}
+}
+
+opcode_impl_ops!(
+    [addr, addi] -> +,
+    [mulr, muli] -> *,
+    [banr, bani] -> &,
+    [borr, bori] -> |
+);
+
+pub fn setr(instruction: &[usize], registers: &[usize]) -> [usize; 4] {
+    assert_len!(instruction);
+    assert_len!(registers);
+
+    let src = instruction[1];
+    let dst = instruction[3];
+
+    let mut output = copy_register(registers);
+
+    output[dst] = output[src];
+    output
+}
+
+pub fn seti(instruction: &[usize], registers: &[usize]) -> [usize; 4] {
+    assert_len!(instruction);
+    assert_len!(registers);
+
+    let src = instruction[1];
+    let dst = instruction[3];
+
+    let mut output = copy_register(registers);
+
+    output[dst] = src;
+    output
+}
+
+macro_rules! opcode_impl_eqs {
+    ([$fn_val_reg:ident, $fn_reg_val:ident, $fn_reg_reg:ident] -> $operation:tt $(,[$fn_val_regs:ident, $fn_reg_vals:ident, $fn_reg_regs:ident] -> $operations:tt)*) => (
+        fn $fn_val_reg(instruction: &[usize], registers: &[usize]) -> [usize; 4] {
+            assert_len!(instruction);
+            assert_len!(registers);
+
+            let lhs = instruction[1];
+            let rhs = instruction[2];
+            let store = instruction[3];
+
+            let mut output = copy_register(registers);
+
+            if lhs $operation registers[rhs] {
+                output[store] = 1;
+            } else {
+                output[store] = 0;
+            }
+
+            output
+        }
+
+        fn $fn_reg_val(instruction: &[usize], registers: &[usize]) -> [usize; 4] {
+            assert_len!(instruction);
+            assert_len!(registers);
+
+            let lhs = instruction[1];
+            let rhs = instruction[2];
+            let store = instruction[3];
+
+            let mut output = copy_register(registers);
+
+            if registers[lhs] $operation rhs {
+                output[store] = 1;
+            } else {
+                output[store] = 0;
+            }
+
+            output
+        }
+
+        fn $fn_reg_reg(instruction: &[usize], registers: &[usize]) -> [usize; 4] {
+            assert_len!(instruction);
+            assert_len!(registers);
+
+            let lhs = instruction[1];
+            let rhs = instruction[2];
+            let store = instruction[3];
+
+            let mut output = copy_register(registers);
+
+            if registers[lhs] $operation registers[rhs] {
+                output[store] = 1
+            } else {
+                output[store] = 0;
+            }
+
+            output
+        }
+
+        opcode_impl_eqs!($([$fn_val_regs, $fn_reg_vals, $fn_reg_regs] -> $operations),*);
+    );
+
+    () => {}
+
+}
+
+opcode_impl_eqs!(
+    [gtir, gtri, gtrr] -> >,
+    [eqir, eqri, eqrr] -> ==
+);
+
+fn copy_register(registers: &[usize]) -> [usize; 4] {
+    assert_len!(registers);
+
+    let mut output = [0; 4];
+    output.copy_from_slice(registers);
+    output
+}
+
+fn parse(input: &str) -> Vec<&str> {
+    input
+        .trim()
+        .lines()
+        .filter(|line| !line.is_empty())
+        .collect()
+}
+
+fn clamp<'a>(
+    iter: impl Iterator<Item = &'a [&'a str]> + 'a,
+) -> impl Iterator<Item = [[usize; 4]; 3]> + 'a {
+    iter.map(move |chunk| match chunk {
+        [before, instruction, after] => {
+            let mut before_array = [0; 4];
+
+            for (src, dst) in before[9..before.len() - 1]
+                .split(", ")
+                .zip(before_array.iter_mut())
+            {
+                *dst = src.parse().unwrap();
+            }
+
+            let mut instruction_array = [0; 4];
+
+            for (src, dst) in instruction.split(' ').zip(instruction_array.iter_mut()) {
+                *dst = src.parse().unwrap();
+            }
+
+            let mut after_array = [0; 4];
+            for (src, dst) in after[9..after.len() - 1]
+                .split(", ")
+                .zip(after_array.iter_mut())
+            {
+                *dst = src.parse().unwrap();
+            }
+
+            [before_array, instruction_array, after_array]
+        }
+        _ => unreachable!(),
+    })
+}
+
+use hashbrown::{HashSet, HashMap};
+
+fn run(mapper: &HashMap<usize, &&str>, opcode: &[usize], registers: &[usize]) -> [usize; 4] {
+    match mapper.get(&opcode[0]).unwrap() {
+        &&"addr" => addr(opcode, registers),
+        &&"addi" => addi(opcode, registers),
+        &&"mulr" => mulr(opcode, registers),
+        &&"muli" => muli(opcode, registers),
+        &&"banr" => banr(opcode, registers),
+        &&"bani" => bani(opcode, registers),
+        &&"borr" => borr(opcode, registers),
+        &&"bori" => bori(opcode, registers),
+        &&"setr" => setr(opcode, registers),
+        &&"seti" => seti(opcode, registers),
+        &&"gtir" => gtir(opcode, registers),
+        &&"gtri" => gtri(opcode, registers),
+        &&"gtrr" => gtrr(opcode, registers),
+        &&"eqir" => eqir(opcode, registers),
+        &&"eqri" => eqri(opcode, registers),
+        &&"eqrr" => eqrr(opcode, registers),
+        _ => unreachable!(),
+    }
+}
 #[aoc(2018, 16, 2)]
-fn main(input: &str) {}
+fn main(input: &str) -> usize {
+    let opcodes = [
+        addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir, eqri,
+        eqrr,
+    ];
+
+    let names = ["addr", "addi", "mulr", "muli", "banr", "bani", "borr", "bori", "setr", "seti", "gtir", "gtri", "gtrr", "eqir", "eqri", "eqrr"];
+
+    let parsed = parse(input);
+    let iterator = clamp(parsed.chunks(3).take_while(|chunk| chunk[0].starts_with("Before")));
+
+    let mut map = HashMap::new();
+
+    for entry in iterator {
+        match entry {
+            [before, instruction, after] => {
+                for (opcode, name) in opcodes.iter().zip(names.iter()) {
+                    if opcode(&instruction, &before)[..] == after[..] {
+                        map.entry(instruction[0]).or_insert_with(HashSet::new).insert(name);
+                    }
+                }
+            }
+        }
+    }
+    
+    let mut newmap = map.clone();
+    let mut finals = HashMap::new();
+    
+    loop {
+        for (code, names) in map.iter() {
+            if names.len() == 1 {
+                for name in names.iter() {
+                    for instruction in newmap.values_mut() {
+                        instruction.remove(name);
+                    }
+                    finals.insert(*code, name.clone());
+                }
+            }
+        }
+
+        map = newmap.clone();
+
+        map.retain(|_, v| v.len() > 0);
+        if map.len() == 0 {
+            break
+        }
+    }
+
+    let mut registers = [0; 4];
+
+    // hehehe, I cheated here.
+    for line in input.lines().skip(3138) {
+        let mut instruction = [0; 4];
+
+        for (src, dst) in line.split(' ').zip(instruction.iter_mut()) {
+            *dst = src.parse().unwrap();
+        }
+
+        registers = run(&finals, &instruction, &registers);
+    }
+
+    registers[0]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_addr() {
+        let instruction = [9, 1, 2, 3];
+        let registers = [10, 20, 30, 40];
+
+        let output = addr(&instruction, &registers);
+        assert_eq!(output, [10, 20, 30, 50]);
+    }
+
+    #[test]
+    fn test_addi() {
+        let instruction = [9, 1, 2, 3];
+        let registers = [10, 20, 30, 40];
+
+        let output = addi(&instruction, &registers);
+        assert_eq!(output, [10, 20, 30, 22]);
+    }
+}
